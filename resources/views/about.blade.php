783ac00f65a1842927b237304c5678b5e13acc6a
mubashar81434 <mubasharsiddique81434@gmail.com>
2025-06-08 21:19:19 +0500

    updating resources/views/about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,8 @@
-<h1>About</h1>
-<x-message-banner/>
+<div>
+    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
+     <h1>About</h1>
+     <x-message-banner 
+        msg="This is in about from message banner" 
+        myTxt="beta"
+     />
+</div>
