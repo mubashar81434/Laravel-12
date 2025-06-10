5014fc491db43616ae8b38a6173c3d71872547e0
mubashar81434 <mubasharsiddique81434@gmail.com>
2025-06-10 16:49:48 +0500

    adding resources/views/form.blade.php

---
+++ resources/views/form.blade.php
@@ -0,0 +1,33 @@
+<div>
+    <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
+     <h4 class="bg-amber-400">User Form</h4>
+     <form action="/about" method="post">
+        <div>
+            <input 
+                type="text" 
+                placeholder="User name"
+                name="user-name"
+            >
+        </div>
+        <div>
+            <input 
+                type="email" 
+                placeholder="User email"
+                name="user-email"
+            >
+        </div>
+        <div>
+            <input 
+                type="password" 
+                placeholder="User password"
+                name="user-password"
+            >
+        </div>
+        <div>
+            <input 
+                type="submit" 
+                value="Register"
+            >
+        </div>
+     </form>
+</div>
