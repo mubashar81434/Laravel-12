3c48fc543fc127f4b1ed25db4a304d40f0bb3feb
mubashar81434 <mubasharsiddique81434@gmail.com>
2025-06-11 19:10:42 +0500

    each and evrrything in request object

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,8 +7,28 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     //
-    function addUser(Request $req){
-        //echo "Add user function called";    
-        return $req;
+    function addUser(Request $req)
+    {
+    //    echo "Add user function";
+       return [
+        'method' => $req->method(),
+        'fullUrl' => $req->fullUrl(),
+        'Validate'=>$req->validate([]),
+        'path' => $req->path(),
+        'ip' => $req->ip(),
+        'isAjax' => $req->ajax(),
+        'headers' => $req->headers->all(),
+        'queryParams' => $req->query(),       // ?key=value
+        'body' => $req->all(),                // POST data / form body
+        'files' => $req->allFiles(),
+        'cookies' => $req->cookies->all(),
+        'server' => $req->server(),
+    ];
+    }
+    function formTwoFun(Request $req)
+    {
+        echo $req['country'];
+        print_r($req->hobbies);
+        // return $req;
     }
 }
