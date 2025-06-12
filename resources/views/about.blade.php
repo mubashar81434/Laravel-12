899544623bedd15de223fe9df0cc3d9570b487f3
mubashar81434 <mubasharsiddique81434@gmail.com>
2025-06-12 19:02:34 +0500

    updating resources/views/about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,13 @@
 <div>
     <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
      <h1>About</h1>
-     <x-message-banner 
+     {{URL::current()}}
+     <h4>{{Request::path()}}</h4>
+     <a href="/home">Home</a>
+     
+     <!-- <x-message-banner 
         msg="This is in about from message banner" 
         myTxt="beta"
-     />
+     /> -->
+
 </div>
