0a5d16d8706e62f28960626eec580be519ecbed3
mubashar81434 <mubasharsiddique81434@gmail.com>
2025-06-12 19:03:06 +0500

    updating resources/views/user.blade.php

--- resources/views/user.blade.php
+++
@@ -1,8 +0,0 @@
-<div>
-    <!-- The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh -->
-     <h1>User</h1>
-     <x-message-banner 
-        msg="This is in user from message banner" 
-        myTxt="alpha"
-     />
-</div>
