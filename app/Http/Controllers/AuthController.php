94e147cbac35235feb4d1b6c05f523e889538524
mubashar81434 <mubasharsiddique81434@gmail.com>
2025-06-12 19:02:16 +0500

    updating app/Http/Controllers/AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,31 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AuthController extends Controller
-{
-    //
-    function addUser(Request $req)
-    {
-    //    echo "Add user function";
-       return [
-        'method' => $req->method(),
-        'fullUrl' => $req->fullUrl(),
-        'Validate'=>$req->validate([]),
-        'path' => $req->path(),
-        'ip' => $req->ip(),
-        'isAjax' => $req->ajax(),
-        'headers' => $req->headers->all(),
-        'queryParams' => $req->query(),       // ?key=value
-        'body' => $req->all(),                // POST data / form body
-        'files' => $req->allFiles(),
-        'cookies' => $req->cookies->all(),
-        'server' => $req->server(),
-    ];
+class AuthController extends Controller{
+
+    function addUser(Request $req){
+        echo "workung";
+        
+        $req->validate([
+            'user-name'=>'required|min:3',
+            'user-email'=>'required|email'
+        ],[
+            "user-name.required"=>"User name should not empty"
+        ]);
+        
+        /*
+        return [
+            'method'=>$req->method(),
+            'fullUrl'=>$req->fullUrl(),
+            'path'=>$req->path(),
+            'ip'=>$req->ip(),
+            'headers'=>$req->headers->all(),
+            'queryParams'=>$req->query(),
+            'body'=>$req->all(),
+            'allFiles'=>$req->allFiles(),
+            'cookies'=>$req->cookies->all(),
+            'sercer'=>$req->server(),
+        ];
+        */
     }
-    function formTwoFun(Request $req)
-    {
+
+    function formTwoFun(Request $req){
         echo $req['country'];
         print_r($req->hobbies);
         // return $req;
     }
+
 }
