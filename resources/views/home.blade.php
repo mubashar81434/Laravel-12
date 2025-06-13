d2095e8b609ed8ae77fc991ed2015b5640b982d4
mubashar81434 <mubasharsiddique81434@gmail.com>
2025-06-13 17:17:55 +0500

    updating resources/views/home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,5 @@
     <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
      <h2>Home</h2>
      <a href={{URL::previous()}}>Back</a>
+     <a href="{{route('hm')}}">Go to user</a>
 </div>
