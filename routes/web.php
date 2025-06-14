87be61613c7f7b45f824b6d16b08d8fc1adea2e4
mubashar81434 <mubasharsiddique81434@gmail.com>
2025-06-14 18:56:34 +0500

    adding routes for prefix and controller group

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,11 @@ Route::get('/', function () {
 
 Route::view('home', 'home');
 
-Route::view('about/user/profile', 'about')->name("hm");
+// Route::view('about/user/profile', 'about')->name("hm");
 
 //Route::get('show', [Home::class,'show']);
 
-Route::view('home/profile/{abc}', 'home')->name('xyz');
+// Route::view('home/profile/{abc}', 'home')->name('xyz');
 
 //Route::view('form','form');
 // Route::post('adduser', [AuthController::class, 'addUser']);
@@ -30,4 +30,25 @@ Route::post('relative-url/{koi b data pass kr skty h}',function($uc-data-ko-yaha
     {{}} // blade template engine ka syntax h 
     // ic mn uc data ko use kr lain gy...
 })
+*/
+
+// route group with prefix
+/*
+Route::prefix('student')->group(function(){
+    Route::get('/detail',[Home::class,'detail']);
+    Route::get('/profile',[Home::class,'profile']);
+});
+*/
+
+// there are three ways to group routes in laravel 
+// i- using prefix group
+// ii- with controllers
+// iii with middlewears 
+
+/*
+Route::controller(home::class)->group(function(){
+    Route::get('/detail',[Home::class,'detail']);
+    Route::get('/profile',[Home::class,'profile']);
+    Route::get('/profile/{name}',[Home::class,'abi-tk-controller-mn-function-ni-bnaya']);
+});
 */
