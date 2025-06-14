e59196b1ddb08084f7ab49c5d8705fa68ec8a883
mubashar81434 <mubasharsiddique81434@gmail.com>
2025-06-14 18:55:57 +0500

    adding the concept of route group via controller and prefix

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class Home extends Controller
 
 {
-    //
+    // 
     function show(){
         // return redirect()->to('about/user/profile');
          return to_route('hm');
@@ -15,6 +15,15 @@ class Home extends Controller
     function user(){
         // return redirect()->to('about/user/profile');
         return to_route('abc', ["name"=>"alpha"]);
+    }
+
+    // route group with prefix function
+
+    function detail(){
+        return "detail called";
+    }
+    function profile(){
+        return "Profile called";
     }
     
 }
